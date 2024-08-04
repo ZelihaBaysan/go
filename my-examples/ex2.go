e67fed013685736291af00800050b4f9cc550c17
Zeliha Baysan <zelihabaysan@posta.mu.edu.tr>
2024-08-04 10:28:28 +0300

    eh

---
+++ my-examples/ex2.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+type Student struct {
+	Name string
+	Age  int
+	ID   string
+}
+
+func NewStudent(name string, age int, id string) *Student {
+	return &Student{Name: name, Age: age, ID: id}
+}
+
+func (s *Student) UpdateAge(newAge int) {
+	s.Age = newAge
+}
+
+func (s *Student) DisplayInfo() {
+	fmt.Printf("Name: %s\nAge: %d\nID: %s\n", s.Name, s.Age, s.ID)
+}
+
+func main() {
+
+	student := NewStudent("Zeliha Baysan", 20, "S12345")
+
+	student.DisplayInfo()
+
+	student.UpdateAge(21)
+
+	student.DisplayInfo()
+}
