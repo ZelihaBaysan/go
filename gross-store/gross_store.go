82b5a55430bfecbc8c861eee34ce7eb2f51b52c3
Zeliha Baysan <zelihabaysan@posta.mu.edu.tr>
2024-08-11 13:59:17 +0300

    new

--- gross-store/gross_store.go
+++ gross-store/gross_store.go
@@ -1,26 +1,51 @@
 package gross
 
-// Units stores the Gross Store unit measurements.
+// Units store the Gross Store unit measurement
 func Units() map[string]int {
-	panic("Please implement the Units() function")
+	return map[string]int{
+		"quarter_of_a_dozen": 3,
+		"half_of_a_dozen":    6,
+		"dozen":              12,
+		"small_gross":        120,
+		"gross":              144,
+		"great_gross":        1728,
+	}
 }
 
-// NewBill creates a new bill.
+// NewBill create a new bill
 func NewBill() map[string]int {
-	panic("Please implement the NewBill() function")
+	return map[string]int{}
 }
 
-// AddItem adds an item to customer bill.
+// AddItem add item to customer bill
 func AddItem(bill, units map[string]int, item, unit string) bool {
-	panic("Please implement the AddItem() function")
+	value, exists := units[unit]
+	if exists {
+		bill[item] += value
+		return true
+	}
+	return false
 }
 
-// RemoveItem removes an item from customer bill.
+// RemoveItem remove item from customer bill
 func RemoveItem(bill, units map[string]int, item, unit string) bool {
-	panic("Please implement the RemoveItem() function")
+	value, exists := units[unit]
+	if !exists {
+		return false
+	}
+
+	if bill[item] >= value {
+		bill[item] -= value
+		if bill[item] == 0 {
+			delete(bill, item)
+		}
+		return true
+	}
+	return false
 }
 
-// GetItem returns the quantity of an item that the customer has in his/her bill.
+// GetItem return the quantity of item that the customer has in his/her bill
 func GetItem(bill map[string]int, item string) (int, bool) {
-	panic("Please implement the GetItem() function")
+	miktar, exist := bill[item]
+	return miktar, exist
 }
