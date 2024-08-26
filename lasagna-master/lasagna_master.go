96b727a8fd8a261c7c072fb1acee6c257dab6ed6
Zeliha Baysan <zelihabaysan@posta.mu.edu.tr>
2024-08-26 11:57:48 +0300

    ndng

--- lasagna-master/lasagna_master.go
+++ lasagna-master/lasagna_master.go
@@ -1,18 +1,37 @@
 package lasagna
 
-// TODO: define the 'PreparationTime()' function
+// PreparationTime calculates the preparation time based on layers and average preparation time per layer.
+func PreparationTime(layers []string, averagePre int) int {
+	if averagePre == 0 {
+		averagePre = 2
+	}
+	return len(layers) * averagePre
+}
 
-// TODO: define the 'Quantities()' function
+// Quantities calculates the quantities of noodles and sauce needed based on the layers provided.
+func Quantities(layers []string) (int, float64) {
+	noodles, sauce := 0, float64(0)
+	for _, layer := range layers {
+		switch layer {
+		case "noodles":
+			noodles += 50
+		case "sauce":
+			sauce += 0.2
+		}
+	}
+	return noodles, sauce
+}
 
-// TODO: define the 'AddSecretIngredient()' function
+// AddSecretIngredient adds the secret ingredient from the friends' list to the end of the user's list.
+func AddSecretIngredient(friendsList []string, myList []string) {
+	myList[len(myList)-1] = friendsList[len(friendsList)-1]
+}
 
-// TODO: define the 'ScaleRecipe()' function
-
-// Your first steps could be to read through the tasks, and create
-// these functions with their correct parameter lists and return types.
-// The function body only needs to contain `panic("")`.
-// 
-// This will make the tests compile, but they will fail.
-// You can then implement the function logic one by one and see
-// an increasing number of tests passing as you implement more 
-// functionality.
+// ScaleRecipe scales the ingredient quantities for the given number of portions.
+func ScaleRecipe(list []float64, portions int) []float64 {
+	sonuc := make([]float64, len(list))
+	for i := 0; i < len(list); i++ {
+		sonuc[i] = list[i] * float64(portions) / 2
+	}
+	return sonuc
+}
